fc56c2717d1a29c154753f5f9acdcbe673791918
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-09-23 18:12:38 +0530

    added list UI generation

---
+++ javaScript/dom/student.js
@@ -0,0 +1,57 @@
+var students = [
+  {
+    name: "jaya",
+
+    gender: "female",
+
+    place: "gachibowli",
+  },
+
+  {
+    name: "dasu",
+
+    gender: "male",
+
+    place: "kphb",
+  },
+
+  {
+    name: "swati",
+
+    gender: "female",
+
+    place: "pune",
+  },
+
+  {
+    name: "pallavi",
+
+    gender: "female",
+
+    place: "assam",
+  },
+
+  {
+    name: "kiran",
+
+    gender: "male",
+
+    place: "kondapur",
+  },
+
+  {
+    name: "thirumala",
+
+    gender: "male",
+
+    place: "madhapur",
+  },
+
+  {
+    name: "priya",
+
+    gender: "female",
+
+    place: "hitech",
+  },
+];
