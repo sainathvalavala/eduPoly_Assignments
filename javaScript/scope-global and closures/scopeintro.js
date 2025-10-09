f0f5ee6616839401d3740c18ed46f4d4637b8d96
Sainathgit config --global user.name Sainathgit config --global user.name Sainath <sainath.valavala.31@gmail.com>
2025-10-09 11:21:00 +0530

    updated closures

--- javaScript/scope-global and closures/scopeintro.js
+++ javaScript/scope-global and closures/scopeintro.js
@@ -17,6 +17,7 @@ for(var i=0;i<=ar.length-1;i++){
 }
 console.log(br);
 br[0]()// gives i value as 3(i is declared with var)
+//gives i as 1( if i is declared with let)
 
 ar.forEach(function(x,y){//creates 2 scopes (global and closure scopes) outer function variable values are binded to inner function variable value under closure scope.
   cr.push(function(){
